17ab89fc42debabc0fc645f26cf243a77efe6ee0
Kaio Vieira <95581947+Tiokaio@users.noreply.github.com>
2022-07-06 10:37:43 -0300

    Add files via upload

---
+++ Aplicação C#/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(21, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 72);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label1.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(48, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label2.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label2.Location = new System.Drawing.Point(52, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Coletor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(12, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Status :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(293, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(21, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "R";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(326, 203);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Transferencia de Arquivos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Aplicação C#/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenNETCF.Desktop.Communication;
+using System.Devices;
+using System.IO;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
+
+namespace Teste
+{
+    
+    public partial class Form1 : Form
+    {
+        
+        RAPI Rapi = new RAPI();
+        RemoteDeviceManager remoteDeviceManager;
+        RemoteDevice dev;
+
+        string ConfigBase = "C:\\CONTROL\\Dados\\MC75\\ClientConfig.txt";
+        string BaseinventBase = "C:\\CONTROL\\Dados\\MC75\\Baseinvent.zip";
+        string BaseinventDestino = @"\Program Files\ColetorNet3\invent\Baseinvent.zip";
+        string ConfigDestino = @"\Program Files\ColetorNet3\Config\ClientConfig.txt";
+        string ColNumber = @"\Program Files\ColetorNet3\Config\ColNumber.txt";
+        string arquivos = @"\Program Files\ColetorNet3\invent\Baseinvent.sdf";
+        string invcnt = @"\Program Files\ColetorNet3\invent\invcnt.txt";
+     
+       
+    
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        
+
+
+        }
+        
+        public void DeleteFile(string deviceFilePath)
+        {
+            if (Rapi.DeviceFileExists(deviceFilePath))
+            {
+                Rapi.DeleteDeviceFile(deviceFilePath);
+            }
+        }
+      
+  
+       
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try {
+                remoteDeviceManager = new RemoteDeviceManager();
+                dev = remoteDeviceManager.Devices.FirstConnectedDevice;
+
+                
+
+                if (dev == null)
+                {
+                    MessageBox.Show("Não Conectado","Coletor:",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                    MessageBox.Show("Insira o coletor e Reinicie a Aplicação","ERRO");
+
+                }
+                 
+                if (Rapi.DevicePresent){
+                    var number = RemoteFile.ReadAllText(dev, ColNumber, Encoding.Default);
+                    var text = number.Replace("Maquina=", ":");
+                    
+                    this.label1.Text = text;
+                }
+                    
+            }catch (Exception ex)
+            {
+               MessageBox.Show("Coletor sem Numero","ERROR:");
+                this.label1.Text=ex.Message;
+            }
+            
+           
+        }
+        
+        public void copiar() { 
+            if (Rapi.DevicePresent)
+            {
+                
+                Rapi.Connect(true);
+                DeleteFile(ConfigDestino);
+                DeleteFile(BaseinventDestino);
+                DeleteFile(arquivos);
+                DeleteFile(invcnt);
+
+
+                RemoteFile.CopyFileToDevice(dev, ConfigBase, ConfigDestino, true);
+                RemoteFile.CopyFileToDevice(dev, BaseinventBase, BaseinventDestino, true);
+                this.label2.Text ="CONCLUIDO";
+               
+                
+            }
+
+            else
+            {
+                MessageBox.Show("Não Foi Possivel Transferir os Arquivos", "Transferencia",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+            return;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            copiar();
+            
+
+        }
+
+
+        
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
